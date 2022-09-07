4db7c8af29d5d11ef087afb980449338b125fa7e
mallick-portfolioE <tamal1714019@gmail.com>
2022-09-07 13:58:00 +0600

    added home product

--- frontend/src/components/Product/ProductCard.jsx
+++ frontend/src/components/Product/ProductCard.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { AiOutlineEye, AiOutlineHeart } from "react-icons/ai";
 import StarRatting from "../Shared/StarRating.jsx";
-import bike1 from "../../assets/images/product/bike1.png";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
     <div className="products-item">
       <div className="product-img-div group">
-        <img className="single-product-img " src={bike1} alt="" />
+        <img className="single-product-img " src={product?.image} alt="" />
         <div className="product-hover-content">
           <div className="product-hover-icon">
             <AiOutlineEye className="text-xl" />
@@ -18,10 +17,10 @@ const ProductCard = () => {
         </div>
       </div>
       <div className="product-content">
-        <h3 className="single-product-title">Black Rear Tailgate Door</h3>
+        <h3 className="single-product-title">{product?.name}</h3>
         <StarRatting rating={5} />
         <div className="price-btn-flex">
-          <p className="card-price">$50</p>
+          <p className="card-price">${product?.price}</p>
           <button className="common-btn2">Buy Now</button>
         </div>
       </div>
--- frontend/src/pages/Home/HomeProduct.jsx
+++ frontend/src/pages/Home/HomeProduct.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductCard from "../../components/Product/ProductCard.jsx";
+import axios from "axios";
+import Loading from "../../components/Shared/Loading";
 const HomeProduct = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const loadProduct = async () => {
+      const { data, status } = await axios.get(
+        "http://localhost:5000/products/home"
+      );
+      if (status === 200) {
+        setProducts(data.data);
+        setLoading(false);
+      } else {
+        setProducts(data.data);
+        setLoading(true);
+      }
+    };
+    loadProduct();
+  }, []);
   return (
     <div className="container pt-24 lg:pt-28">
       <div className="text-center">
@@ -8,8 +27,13 @@ const HomeProduct = () => {
         <h2 className="text-4xl font-semibold">Best Seller</h2>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5 lg:gap-8">
-        <ProductCard />
-        
+        {loading ? (
+          <Loading />
+        ) : (
+          products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))
+        )}
       </div>
     </div>
   );
