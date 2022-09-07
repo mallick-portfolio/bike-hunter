88bd65a5ce9d991ad8128f177b7b37272e84d25d
mallick-portfolioE <tamal1714019@gmail.com>
2022-09-07 11:12:25 +0600

    added start Ratgin

--- frontend/src/pages/Home/HomeProduct.jsx
+++ frontend/src/pages/Home/HomeProduct.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import bike1 from "../../assets/images/product/bike1.png";
 const HomeProduct = () => {
   return (
     <div className="container pt-24 lg:pt-28">
@@ -8,7 +8,30 @@ const HomeProduct = () => {
         <h2 className="text-4xl font-semibold">Best Seller</h2>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <h1>hello wrodl</h1>
+      <div className="foods-product-item">
+      <div className="product-img-div group">
+        <img className="single-product-img " src={product?.images} alt="" />
+        <div className="product-hover-content">
+          <div className="product-hover-icon">
+            <AiOutlineShoppingCart />
+          </div>
+          <div className="product-hover-icon">
+            <AiOutlineEye />
+          </div>
+          <div className="product-hover-icon">
+            <AiOutlineHeart />
+          </div>
+        </div>
+      </div>
+      <div className="product-content">
+        <h3 className="single-product-title">{product?.title}</h3>
+        <StarRatting rating={product?.avgRatting} />
+        <div className="price-btn-flex">
+          <p className="card-price">${product?.price}</p>
+          <button className="add-to-cart-btn">Add to Cart</button>
+        </div>
+      </div>
+    </div>
       </div>
     </div>
   );
