9a51ab1190605c3d11d44ef5bde59239859395b1
mallick-portfolioE <tamal1714019@gmail.com>
2022-09-07 11:01:43 +0600

    update ..

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import { HomeBanner } from "../../components/index.js";
 import DiscountBanner from "./DiscountBanner.jsx";
 import FeatureCategory from "./FeatureCategory.jsx";
 import FeatureItem from "./FeatureItem.jsx";
+import HomeProduct from "./HomeProduct.jsx";
 
 const Home = () => {
   return (
     <div>
       <HomeBanner />
       <FeatureCategory />
+      <HomeProduct />
       <FeatureItem />
       <DiscountBanner />
     </div>
---
+++ frontend/src/pages/Home/HomeProduct.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeProduct = () => {
+  return (
+    <div className="container pt-24 lg:pt-28">
+      <div className="text-center">
+        <h4 className="text-lg lg:text-xl">YOUR RIDE START HERE.</h4>
+        <h2 className="text-4xl font-semibold">Best Seller</h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <h1>hello wrodl</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomeProduct;
