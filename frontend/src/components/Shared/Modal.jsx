a02fc662f4f2e76bfc71ecdc459f6e8eeba6590d
mallick-portfolioE <tamal1714019@gmail.com>
2022-09-08 11:10:20 +0600

    updated modal

--- frontend/src/components/Shared/Modal.jsx
+++ frontend/src/components/Shared/Modal.jsx
@@ -18,57 +18,41 @@ const Modal = ({ item }) => {
     };
   }, [item]);
   return (
-    <div className="flex justify-center items-center h-auto lg:h-screen fixed top-0 left-0 bg-menu w-screen right-0 bottom-0  z-50 overflow-scroll lg:overflow-hidden">
+    <div className="modal-container">
       <div className="mt-64 md:mt-0">
-        <div className="lg:w-2/3 w-full flex flex-col md:flex-row justify-between gap-6 p-4 lg:p-12 mx-auto bg-btn border border-primary rounded-2xl">
-          <div className="w-full md:w-1/2 p-4 bg-btn shadow-3xl rounded-lg">
+        <div className="modal-in">
+          <div className="modal-col">
             <img className="w-44 mx-auto" src={data.image} alt="" />
-            <h1 className="text-primary text-xl lg:text-2xl font-serif">
-              {data.name}
-            </h1>
+            <h1 className="modal-product-title">{data.name}</h1>
             <p className="text-base font-medium py-2 text-tertiary">
               {data.des.length > 110 ? data.des.slice(0, 110) : data.des}
             </p>
-            <p className="flex justify-between items-center py-0.5">
-              <span className="text-primary text-base font-semibold">
-                Category
-              </span>{" "}
-              <span className="text-primary text-xl font-semibold">
-                {data.category}{" "}
-              </span>
+            <p className="modal-paragraph">
+              <span className="modal-span">Category</span>{" "}
+              <span className="modal-subSpan">{data.category} </span>
             </p>
-            <p className="flex justify-between items-center py-0.5">
-              <span className="text-primary text-base font-semibold">
+            <p className="modal-paragraph">
+              <span className="modal-span">
                 Price
                 <span className="text-xs text-tertiary ml-1">
                   (Per Product)
                 </span>
               </span>{" "}
-              <span className="text-primary text-xl font-semibold">
-                ${data.price}{" "}
-              </span>
+              <span className="modal-subSpan">${data.price} </span>
             </p>
-            <p className="flex justify-between items-center py-0.5">
-              <span className="text-primary text-base font-semibold">
-                Available Quantity
-              </span>{" "}
-              <span className="text-primary text-xl font-semibold">
-                {data.avilQty}
-              </span>
+            <p className="modal-paragraph">
+              <span className="modal-span">Available Quantity</span>{" "}
+              <span className="modal-subSpan">{data.avilQty}</span>
             </p>
-            <p className="flex justify-between items-center py-0.5">
-              <span className="text-primary text-base font-semibold">
-                Minimum Ordered Quantity
-              </span>{" "}
-              <span className="text-primary text-xl font-semibold">
-                {data.minQty}
-              </span>
+            <p className="modal-paragraph">
+              <span className="modal-span">Minimum Ordered Quantity</span>{" "}
+              <span className="modal-subSpan">{data.minQty}</span>
             </p>
             <button onClick={() => dispatch(closeModal())}>
               <FontAwesomeIcon className="xicon" icon={faXmark} />
             </button>
           </div>
-          <div className="w-full md:w-1/2 p-4 bg-btn shadow-3xl rounded-lg">
+          <div className="modal-col">
             <form action="">
               <div className="my-1 lg:my-2">
                 <label className="form-label" htmlFor="">
