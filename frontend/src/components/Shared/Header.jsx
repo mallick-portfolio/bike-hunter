498a323221e6048dc74ef8d77e8bd6498a878ed4
mallick-portfolioE <tamal1714019@gmail.com>
2022-09-09 16:02:17 +0600

    updated ..

--- frontend/src/components/Shared/Header.jsx
+++ frontend/src/components/Shared/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faBars } from "@fortawesome/free-solid-svg-icons";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -8,122 +8,144 @@ import { signOut } from "firebase/auth";
 import Loading from "./Loading.jsx";
 import useAdmin from "../../hooks/useAdmin.js";
 const Header = () => {
-  const navigate = useNavigate();
   const [user, loading] = useAuthState(auth);
-  const [admin, adminLoading] = useAdmin(user);
+  const [admin] = useAdmin(user);
   const [show, setShow] = useState(false);
   const [active, setActive] = useState(false);
-  if (loading || adminLoading) {
+  const [stickyClass, setStickyClass] = useState("relative");
+
+  useEffect(() => {
+    window.addEventListener("scroll", stickNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", stickNavbar);
+    };
+  }, []);
+
+  const stickNavbar = () => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      windowHeight > 500
+        ? setStickyClass("fixed top-0 bg-white w-full right left-0 z-50")
+        : setStickyClass("relative");
+    }
+  };
+
+  if (loading) {
     return <Loading />;
   }
   return (
-    <div className="header-container">
-      <div>
-        <h1 className="text-2xl font-medium text-primary">
-          <Link to="/">
-            Bike <span className="text-bodyWhite cursor-pointer">Hunter</span>
-          </Link>
-        </h1>
-      </div>
-      <div className="menu">
-        {/* Desktop Menu */}
-        <div className="hidden lg:flex">
-          <ul className="desktop-ul">
-            {menus.map((item) => (
-              <li key={item.id}>
-                <Link className="nav-item" to={item.route}>
-                  {item.display}
-                </Link>
-              </li>
-            ))}
-          </ul>
+    <div className={`${stickyClass}`}>
+      <div className={`header-container`}>
+        <div>
+          <h1 className="text-2xl font-medium text-primary">
+            <Link to="/">
+              Bike <span className="text-bodyWhite cursor-pointer">Hunter</span>
+            </Link>
+          </h1>
         </div>
-        <div className="hidden lg:flex">
-          <ul className="flex items-center gap-6">
-            {user?.email ? (
-              <>
-                <li onClick={() => setActive(!active)} className="relative">
-                  <img
-                    className="userImage"
-                    src={
-                      user?.photoURL
-                        ? user?.photoURL
-                        : "https://placeimg.com/192/192/people"
-                    }
-                    alt=""
-                  />
-                  <div
-                    className={`absolute ${
-                      active ? "top-16 right-0" : "-top-[600px] right-0"
-                    } dropdown-menu`}
-                  >
-                    {admin ? adminItem : usersItem}
-                  </div>
-                </li>
-              </>
-            ) : (
-              <li>
-                <Link className="common-btn" to={"/login"}>
-                  Login
-                </Link>
-              </li>
-            )}
-          </ul>
-        </div>
-        <div className="flex items-center gap-4 lg:hidden">
-          <div className="relative">
-            <button onClick={() => setActive(!active)}>
-              <img
-                className="userImage"
-                src={
-                  user?.photoURL
-                    ? user?.photoURL
-                    : "https://placeimg.com/192/192/people"
-                }
-                alt=""
-              />
-            </button>
-            <div
-              className={`absolute ${
-                active ? "top-16 right-0" : "-top-[600px] right-0"
-              } dropdown-menu`}
-            >
-              {admin ? adminItem : usersItem}
-            </div>
-          </div>
-          <button onClick={() => setShow(!show)}>
-            <FontAwesomeIcon className="xicon" icon={faBars} />
-          </button>
-        </div>
-        {/* Mobile Menu */}
-        <div
-          className={`mobile-menu ${
-            show
-              ? "top-0 left-0 bg-menu"
-              : "top-0 -left-[1000px] bg-transparent"
-          }`}
-        >
-          <div className="menu-head">
-            <div className="mobile-logo-area">
-              <h1 className="text-2xl font-bold text-primary">
-                <Link to="/">
-                  Bike{" "}
-                  <span className="text-bodyWhite cursor-pointer">Hunter</span>
-                </Link>
-              </h1>
-              <button onClick={() => setShow(!show)}>
-                <FontAwesomeIcon className="xicon" icon={faXmark} />
-              </button>
-            </div>
-            <ul className="mobile-menu-ul">
+        <div className="menu">
+          {/* Desktop Menu */}
+          <div className="hidden lg:flex">
+            <ul className="desktop-ul">
               {menus.map((item) => (
                 <li key={item.id}>
-                  <Link className="mobile-nav-item" to={item.route}>
+                  <Link className="nav-item" to={item.route}>
                     {item.display}
                   </Link>
                 </li>
               ))}
             </ul>
+          </div>
+          <div className="hidden lg:flex">
+            <ul className="flex items-center gap-6">
+              {user?.email ? (
+                <>
+                  <li onClick={() => setActive(!active)} className="relative">
+                    <img
+                      className="userImage"
+                      src={
+                        user?.photoURL
+                          ? user?.photoURL
+                          : "https://placeimg.com/192/192/people"
+                      }
+                      alt=""
+                    />
+                    <div
+                      className={`absolute ${
+                        active ? "top-16 right-0" : "-top-[600px] right-0"
+                      } dropdown-menu`}
+                    >
+                      {admin ? adminItem : usersItem}
+                    </div>
+                  </li>
+                </>
+              ) : (
+                <li>
+                  <Link className="common-btn" to={"/login"}>
+                    Login
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+          <div className="flex items-center gap-4 lg:hidden">
+            <div className="relative">
+              <button onClick={() => setActive(!active)}>
+                <img
+                  className="userImage"
+                  src={
+                    user?.photoURL
+                      ? user?.photoURL
+                      : "https://placeimg.com/192/192/people"
+                  }
+                  alt=""
+                />
+              </button>
+              <div
+                className={`absolute ${
+                  active ? "top-16 right-0" : "-top-[600px] right-0"
+                } dropdown-menu`}
+              >
+                {admin ? adminItem : usersItem}
+              </div>
+            </div>
+            <button onClick={() => setShow(!show)}>
+              <FontAwesomeIcon className="xicon" icon={faBars} />
+            </button>
+          </div>
+          {/* Mobile Menu */}
+          <div
+            className={`mobile-menu ${
+              show
+                ? "top-0 left-0 bg-menu"
+                : "top-0 -left-[1000px] bg-transparent"
+            }`}
+          >
+            <div className="menu-head">
+              <div className="mobile-logo-area">
+                <h1 className="text-2xl font-bold text-primary">
+                  <Link to="/">
+                    Bike{" "}
+                    <span className="text-bodyWhite cursor-pointer">
+                      Hunter
+                    </span>
+                  </Link>
+                </h1>
+                <button onClick={() => setShow(!show)}>
+                  <FontAwesomeIcon className="xicon" icon={faXmark} />
+                </button>
+              </div>
+              <ul className="mobile-menu-ul">
+                {menus.map((item) => (
+                  <li key={item.id}>
+                    <Link className="mobile-nav-item" to={item.route}>
+                      {item.display}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
