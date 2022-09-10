3184efcaf5a94b397a7cf83c183b18c3bc293d58
mallick-portfolioE <tamal1714019@gmail.com>
2022-09-10 11:42:00 +0600

    update ...

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,7 @@
 export { default as Home } from "./Home/Home.jsx";
 export { default as About } from "./About/About.jsx";
-export { default as Blog } from "./Blog.jsx";
-export { default as Contact } from "./Contact.jsx";
+export { default as Blog } from "./Blog/Blog.jsx";
+export { default as Contact } from "./Contact/Contact.jsx";
 export { default as Login } from "./login/Login.jsx";
 export { default as PrivateRoute } from "./login/PrivateRoute.jsx";
 export { default as PageNotFound } from "./PageNotFound.jsx";
